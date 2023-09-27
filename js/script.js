ba9619e439960c8077ef219a3570e151571ec68a
Oleksandr Zheliabin <oleksandr.zheliabin@gmail.com>
2023-09-26 23:33:27 -0700

    Completed a few steps of assignment

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// STEP 1 - Convert the following highlighted identifiers to Camel Case notation:
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let yLibraryFunction
+
+// STEP 2 - Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+
+// numeric literal expression - 3.14
+// string literal expression - 'Oleksandr'
+// Boolean literal expression - true
+// null literal expression - null
+
+// STEP 3 - Give me two examples of complex / variable expressions.
+
+// let firstExpression = 2 * 5 - 3
+// let secondExpression = (3 + 5) / (2 * 2)
+
+// STEP 4 - Declare (but do not assign) 9 variables for the following identifiers: 
+//          First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. 
+//          Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+// let firstName
+// let lastName
+// let strAddress
+// let strCity
+// let strState
+// let strZipCode
+// let yourAge
+// let referralSource
+// let bMayWeContactYou
+
+// STEP 5 - Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+
+// let firstName
+// let lastName
+// let strCity
+// firstName = 'Oleksandr'
+// lastName = 'Zheliabin'
+// strCity = 'San Diego'
+
+// let firstName = 'Oleksandr'
+// let lastName = 'Zheliabin'
+// let strCity = 'San Diego'
+
+// let firstName = 'Oleksandr', lastName = 'Zheliabin', strCity = 'San Diego'
+
+// STEP 6 - Create a variable. Add a number and a string and display the coerced result in the browser’s console window.
+
+let yourAge = 40
+alert('I am ' + yourAge + ' years old.')
